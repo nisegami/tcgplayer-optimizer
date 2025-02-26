57cd416a5e42175fbea4fdd95dfce3babac54a26
Arshad <arsh@dhose.in>
2025-02-26 11:07:08 -0500

    configured eslint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,27 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
 
-  modules: [
-    '@nuxt/ui',
-    '@nuxt/eslint'
-  ],
+    modules: [
+        '@nuxt/ui',
+        '@nuxt/eslint',
+    ],
+    devtools: { enabled: true },
 
-  css: ['~/assets/css/main.css'],
+    css: ['~/assets/css/main.css'],
 
-  future: {
-    compatibilityVersion: 4
-  },
+    future: {
+        compatibilityVersion: 4,
+    },
 
-  compatibilityDate: '2024-11-27'
+    compatibilityDate: '2024-11-27',
+
+    eslint: {
+        config: {
+            stylistic: {
+                indent: 4,
+                quotes: 'single',
+                semi: false,
+            },
+        },
+    },
 })
